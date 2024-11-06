fae7fd1ff9ae3771cd90230dbdb508dfaae8c165
viniz0x <armazenamentovini@gmail.com>
2024-11-06 11:08:05 -0300

    build: configuracao_entity_tabela

---
+++ Infraestrutura/Db/DbContexto.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Entidades.DTOs;
+
+namespace MinimalApi.Infraestrutura.Db;
+
+public class DbContexto : DbContext{
+
+    private readonly IConfiguration _configurationAppSettings;
+    public DbContexto(IConfiguration configurationAppSettings) 
+    {
+        _configurationAppSettings = configurationAppSettings;
+    }
+    public DbSet<Administrador> Administradores { get; set; } =default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured) 
+        {
+            var stringConexao = _configurationAppSettings.GetConnectionString("mysql")?.ToString();
+            if(!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(stringConexao, 
+                ServerVersion.AutoDetect(stringConexao));
+            }
+        }
+    }
+}
