b74f7abcfd1a0cd57d5e19644cfd80deb7e0ea72
viniz0x <armazenamentovini@gmail.com>
2024-11-11 14:39:25 -0300

    build: post_criado_para_veiculos

--- Program.cs
+++ Program.cs
@@ -5,10 +5,13 @@ using MinimalApi.Dominio.Interfaces;
 using MinimalApi.Dominio.Servicos;
 using Microsoft.AspNetCore.Mvc;
 using MinimalApi.Dominios.Models;
+using MinimalApi.Entidades.DTOs;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServicio>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
 
@@ -21,22 +24,42 @@ builder.Services.AddDbContext<DbContexto>(options => {
         );
 });
 
-
 var app = builder.Build();
+#endregion
 
-
-
+#region Home
 app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => { 
+#region Administradores
+app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => { 
     if(administradorServico.Login(loginDTO) != null){
         return Results.Ok("Login realizado com sucesso!");
     } else {
         return Results.Unauthorized();
     }
 });
+#endregion
 
+#region Veiculos
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) => { 
+    
+    var veiculo = new Veiculo {
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano
+    };
+
+    veiculoServico.Incluir(veiculo);
+
+    return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
+
+});
+#endregion
+
+#region App
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+#endregion
--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2afc63b3b534df2a15977ecd804c541ef10d5992")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb38d76204852e62bf584fd674aba52a1a07c968")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
