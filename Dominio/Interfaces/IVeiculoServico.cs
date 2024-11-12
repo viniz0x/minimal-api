722080577cc2178cf6fe83e9e59f80ac5f669e5f
viniz0x <armazenamentovini@gmail.com>
2024-11-12 18:40:06 -0300

    build: get_busca_de_veiculos

--- Dominio/Interfaces/IVeiculoServico.cs
+++ Dominio/Interfaces/IVeiculoServico.cs
@@ -6,7 +6,7 @@ namespace MinimalApi.Dominio.Interfaces;
 
 public interface IVeiculoServico
 {
-    List<Veiculo> Todos(int pagina = 1, string? nome = null, string? marca = null);
+    List<Veiculo> Todos(int? pagina = 1, string? nome = null, string? marca = null);
 
     Veiculo? BuscarPorId(int id);
 
